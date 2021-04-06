cb540f51d264e56022c868fe2b18ee0c1d32bd5d
Anthony Merante <44046970+AnthonyJM18@users.noreply.github.com>
2021-04-06 11:02:18 -0400

    Prefix created

--- Project2_Group_4/Conversions/PrefixConversion.cs
+++ Project2_Group_4/Conversions/PrefixConversion.cs
@@ -8,5 +8,84 @@ namespace Project2_Group_4.Conversions
 {
     public class PrefixConversion
     {
+        private List<Data> Dataset;
+
+        public PrefixConversion(List<Data> d)
+        {
+            Dataset = d;
+        }
+
+        public IEnumerable<string> Convert()
+        {
+            try
+            {
+
+                StringBuilder equation = new StringBuilder();
+                Stack<char> command;
+                foreach (Data d in Dataset)
+                {
+                    command = new Stack<char>();
+                    equation.Clear();
+                    string expression = new string(d.Infix.Reverse().ToArray());
+                    foreach (char c in expression)
+                    {
+                        if (char.IsDigit(c))
+                        {
+                            equation.Append(c);
+                        }
+                        else
+                        {
+                            if (c == ')')
+                            {
+                                command.Push(c);
+                            }
+                            else if (c == '(')
+                            {
+                                while (command.Peek() != ')')
+                                {
+                                    equation.Append(command.Pop());
+                                }
+                                command.Pop();
+                            }
+                            else
+                            {
+                                while (command.Count > 0 && GetPrecedence(command.Peek()) > GetPrecedence(c))
+                                {
+                                    equation.Append(command.Pop());
+                                }
+                                command.Push(c);
+                            }
+                        }
+                    }
+                    while (command.Count > 0)
+                    {
+                        equation.Append(command.Pop());
+                    }
+                    expression = new string(equation.ToString().Reverse().ToArray());
+                    yield return expression;
+                }
+            }
+            finally
+            {
+
+            }
+        }
+        private int GetPrecedence(char ch)
+        {
+            switch (ch)
+            {
+                case '+':
+                case '-':
+                    return 1;
+
+                case '*':
+                case '/':
+                    return 2;
+
+                case '^':
+                    return 3;
+            }
+            return -1;
+        }
     }
 }
