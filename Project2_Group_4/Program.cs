6253398fbe652147d4d949d5318c3ade210171e9
Colin Manliclic <c_manliclic@fanshaweonline.ca>
2021-04-06 14:31:07 -0400

    Can now open browser

--- Project2_Group_4/Program.cs
+++ Project2_Group_4/Program.cs
@@ -4,6 +4,7 @@ using Project2_Group_4.Expressions;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Diagnostics;
 
 namespace Project2_Group_4
 {
@@ -12,7 +13,7 @@ namespace Project2_Group_4
         static void Main(string[] args)
         {
             const string DATA_PATH = "./Data/Project 2_INFO_5101.csv";
-            const string XML_PATH = "../../../Data/Project2_INFO_5101.xml";
+            const string XML_PATH = "./Data/Project2_INFO_5101.xml";
             // Load data
             List<Data> dataset = CSVFile.CSVDeserialize(DATA_PATH);
 
@@ -75,16 +76,22 @@ namespace Project2_Group_4
                 outputFile.WriteStartDocument();
                 outputFile.WriteStartRootElement();
 
-                outputFile.WriteStartElement();
-                outputFile.WriteAttribute("sno", "1");
-                outputFile.WriteEndElement();
-
-
+                foreach(Data d in dataset)
+                {
+                    outputFile.WriteStartElement();
+                    outputFile.WriteAttribute("sno", d.Sno.ToString());
+                    outputFile.WriteAttribute("infix", d.Infix);
+                    outputFile.WriteAttribute("prefix", d.Prefix);
+                    outputFile.WriteAttribute("postfix", d.Postfix);
+                    outputFile.WriteAttribute("evaluation", d.PostfixResult);
+                    outputFile.WriteAttribute("comparison", (ce.Compare(d.PrefixResult, d.PostfixResult) == 1 ? "True" : "False"));
+                    outputFile.WriteEndElement();
+                }
+                
                 outputFile.WriteEndRootElement();
             }
 
-            string absolutePath = Path.GetFullPath(XML_PATH);
-            System.Diagnostics.Process.Start("chrome.exe", $"\"{absolutePath}\"");
+            Process.Start(@"cmd.exe ", @$"/c start chrome {XML_PATH}");
         }
     }
 }
