1ef227d4a9c34b365eac3b656858f301355c0fc1
zinchop <zina_long@hotmail.com>
2021-04-06 10:52:31 -0400

    changed operand to char

--- Project2_Group_4/Expressions/ExpressionEvaluation.cs
+++ Project2_Group_4/Expressions/ExpressionEvaluation.cs
@@ -9,22 +9,22 @@ namespace Project2_Group_4.Expressions
 {
     class ExpressionEvaluation
     {
-        public string EvaluateExpression(int left, int right, string operand)
+        public string EvaluateExpression(int left, int right, char operand)
         {
             var leftNode = Expression.Constant(left);
             var rightNode = Expression.Constant(right);
             switch(operand)
             {
-                case "+":
+                case '+':
                     var additionExpression = Expression.Add(leftNode, rightNode);
                     return Expression.Lambda<Func<int>>(additionExpression).Compile().ToString();
-                case "-":
+                case '-':
                     var subtractionExpression = Expression.Subtract(leftNode, rightNode);
                     return Expression.Lambda<Func<int>>(subtractionExpression).Compile().ToString();
-                case "*":
+                case '*':
                     var multiplyExpression = Expression.Multiply(leftNode, rightNode);
                     return Expression.Lambda<Func<int>>(multiplyExpression).Compile().ToString();
-                case "/":
+                case '/':
                     var divideExpression = Expression.Divide(leftNode, rightNode);
                     return Expression.Lambda<Func<int>>(divideExpression).Compile().ToString();
                 default:
