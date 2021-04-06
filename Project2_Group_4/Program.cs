02e329beb9e27428f4427a8d716f991b1b5070ff
Anthony Merante <44046970+AnthonyJM18@users.noreply.github.com>
2021-04-06 13:44:00 -0400

    finished output

--- Project2_Group_4/Program.cs
+++ Project2_Group_4/Program.cs
@@ -14,30 +14,25 @@ namespace Project2_Group_4
             // Load data
             List<Data> dataset = CSVFile.CSVDeserialize(DATA_PATH);
 
+            Console.WriteLine("\nSno \tInfix");
+            foreach (Data result in dataset)
+            {
+                Console.WriteLine(result.Sno + ":\t" + result.Infix);
+            }
             // Convert to postfix and store it in the data
             PostfixConversion post = new PostfixConversion(dataset);
             int count = 0;
-            Console.WriteLine("Sno \tPostfix");
+            Console.WriteLine("\nSno \tPostfix");
             foreach (string result in post.Convert())
             {
                 dataset[count].Postfix = result;
                 Console.WriteLine(dataset[count].Sno + ":\t" + dataset[count].Postfix);
                 count++;
             }
-            //Postfix evaluation
-            count = 0;
-            Console.WriteLine("Sno \tPostfixEVAL");
-            foreach (string result in ExpressionEvaluation.PostFixEvaluate(post.Convert()))
-            {
-                dataset[count].PostfixResult = result;
-                Console.WriteLine(dataset[count].Sno + ":\t" + result);
-                count++;
-            }
-
             // Convert to prefix and store it in the data
             PrefixConversion pre = new PrefixConversion(dataset);
             count = 0;
-            Console.WriteLine("Sno \tPostfix");
+            Console.WriteLine("\nSno \tPrefix");
             foreach (string result in pre.Convert())
             {
                 dataset[count].Prefix = result;
@@ -46,7 +41,7 @@ namespace Project2_Group_4
             }
             // Calculate postfix result and store it in the data
             count = 0;
-            Console.WriteLine("Sno \tPostFix Res");
+            Console.WriteLine("\nSno \tPostFix Results");
             foreach (string result in ExpressionEvaluation.PostFixEvaluate(post.Convert()))
             {
                 dataset[count].PostfixResult = result;
@@ -56,7 +51,7 @@ namespace Project2_Group_4
 
             // Calculate prefix result and store it in the data
             count = 0;
-            Console.WriteLine("Sno \tPrefix Res");
+            Console.WriteLine("\nSno \tPrefix Results");
             foreach (string result in ExpressionEvaluation.PreFixEvaluate(pre.Convert()))
             {
                 dataset[count].PrefixResult = result;
@@ -64,13 +59,9 @@ namespace Project2_Group_4
                 count++;
             }
 
-            // Display prefix results 
-
-            // Display postfix results
-
             // Display all results
             CompareExpressions ce = new CompareExpressions();
-            Console.WriteLine($"Sno\t{"Infix",-20} {"Prefix",-17} {"Postfix",-17} {"Prefix Result",-15} {"Postfix Result",-15} {"Match",-15}");
+            Console.WriteLine($"\nSno\t{"Infix",-20} {"Prefix",-17} {"Postfix",-17} {"Prefix Result",-15} {"Postfix Result",-15} {"Match",-15}");
             foreach (Data d in dataset)
             {
                 Console.WriteLine($"{d.Sno}\t{d.Infix,-20} {d.Prefix,-17} {d.Postfix,-17} {d.PrefixResult,-15} {d.PostfixResult,-15} {(ce.Compare(d.PrefixResult, d.PostfixResult) == 1 ? "True" : "False"),-15}");
