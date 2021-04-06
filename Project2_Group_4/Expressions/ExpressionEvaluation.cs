b25aad0bd46f5de3f5f81d9941b59159e3ac9f66
zinchop <zina_long@hotmail.com>
2021-04-06 10:50:37 -0400

    EvaluateExpression method

--- Project2_Group_4/Expressions/ExpressionEvaluation.cs
+++ Project2_Group_4/Expressions/ExpressionEvaluation.cs
@@ -3,10 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Linq.Expressions;
 
 namespace Project2_Group_4.Expressions
 {
     class ExpressionEvaluation
     {
+        public string EvaluateExpression(int left, int right, string operand)
+        {
+            var leftNode = Expression.Constant(left);
+            var rightNode = Expression.Constant(right);
+            switch(operand)
+            {
+                case "+":
+                    var additionExpression = Expression.Add(leftNode, rightNode);
+                    return Expression.Lambda<Func<int>>(additionExpression).Compile().ToString();
+                case "-":
+                    var subtractionExpression = Expression.Subtract(leftNode, rightNode);
+                    return Expression.Lambda<Func<int>>(subtractionExpression).Compile().ToString();
+                case "*":
+                    var multiplyExpression = Expression.Multiply(leftNode, rightNode);
+                    return Expression.Lambda<Func<int>>(multiplyExpression).Compile().ToString();
+                case "/":
+                    var divideExpression = Expression.Divide(leftNode, rightNode);
+                    return Expression.Lambda<Func<int>>(divideExpression).Compile().ToString();
+                default:
+                    return "ERROR: Operand does not exist.";
+            }
+        }
     }
 }
